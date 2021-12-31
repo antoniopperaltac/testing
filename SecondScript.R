069b7b63c647d67c8c0df3bc67bacf08dac0288e
appcpf <antoniop94@gmail.com>
2021-12-31 00:46:09 -0400

    En este cambio creamos el data frame lista de productos a traves del archivo script.

---
+++ SecondScript.R
@@ -0,0 +1,10 @@
+# Probando con un 2do Archivo de Script (coding)
+
+items<- c(1,2,3,4,5)
+
+products<-c("milk","cornflakes","butter","bread","cheese")
+
+prod_list <- data.frame(items,products)
+
+prod_list
+
